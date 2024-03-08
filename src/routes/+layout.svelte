a153a0da249d6f02ad0556a3f9aa7fc5af639190
Suyeon Kim <alex@finddy.io>
2024-03-08 14:19:53 +0900

    5. 최상단 레이아웃을 <ParaglideJS /> 컴포넌트로 감싸기

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { i18n } from '$lib/i18n.js';
+	import { ParaglideJS } from '@inlang/paraglide-js-adapter-sveltekit';
+</script>
+
+<!-- 
+	This will do a few things for you:
+1. Automatically set the language for Paraglide
+2. Automatically add rel="alternate" links to your page head
+3. Automatically translate any <a href> attributes on your page. You don't need to change them
+-->
+<ParaglideJS {i18n}>
+	<slot />
+</ParaglideJS>
