7ffc60c1d4949f9f95e6feb013667df4dbc34446
Suyeon Kim <alex@finddy.io>
2024-03-08 14:16:24 +0900

    3. paraglide alias 설정 & postinstall 커맨드 실행

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+
+		// * alias 설정 후에 pnpm run postinstall 로 inlang 한 번 실행시켜주기
+		alias: {
+			$paraglide: './src/paraglide'
+		}
 	}
 };
 
