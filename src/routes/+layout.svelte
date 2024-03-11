a57683f806be86116fc00307b5dea85e4e167ad5
Suyeon Kim <alex@finddy.io>
2024-03-11 14:07:45 +0900

    tailwind CSS 설치 및 환경설정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script>
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-js-adapter-sveltekit';
+	import '../app.css';
 </script>
 
-<!-- 
-	This will do a few things for you:
-1. Automatically set the language for Paraglide
-2. Automatically add rel="alternate" links to your page head
-3. Automatically translate any <a href> attributes on your page. You don't need to change them
--->
 <ParaglideJS {i18n}>
 	<slot />
 </ParaglideJS>
