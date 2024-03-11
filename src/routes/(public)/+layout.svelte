f362cdf1e909d9954851e1fc4e3d6a285f0a2b24
Suyeon Kim <alex@finddy.io>
2024-03-11 15:27:18 +0900

    inter 폰트 설치, global CSS variables 추가

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import '@fontsource-variable/inter';
+</script>
+
 <!-- 
   * TODO: 물어보기
   * TODO - 1. what-we-do 상세 내용은 다이나믹?
@@ -28,4 +32,30 @@
 <main>
 	<slot />
 </main>
+
 <!-- footer -->
+
+<style>
+	:global(body) {
+		font-family: 'Inter', sans-serif;
+	}
+
+	:global(:root) {
+		--font-regular: 400;
+		--font-medium: 500;
+		--font-bold: 700;
+
+		--color-primary: #3c41be;
+		--color-secondary: #f1f1f1;
+		--color-accent: #0a39c3;
+		--color-text-primary: #3c3b38;
+		--color-black: #000000;
+		--color-white: #ffffff;
+		--color-gray-light: #f4f3f0;
+
+		--leading-snug: 1.3;
+		--leading-normal: 1.5;
+
+		--tracking-tighter: -0.05em;
+	}
+</style>
