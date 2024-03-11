c08b789eb63205f5a3f527dda109d0beebf3c680
Suyeon Kim <alex@finddy.io>
2024-03-11 14:47:00 +0900

    1차 & 2차 취합본 라우팅 파일들 생성

---
+++ src/routes/(public)/+layout.svelte
@@ -0,0 +1,31 @@
+<!-- 
+  * TODO: 물어보기
+  * TODO - 1. what-we-do 상세 내용은 다이나믹?
+  [navigation bar]
+  - about
+    - what-we-do
+      - [slug]
+    - who-we-are
+    - people
+      - [slug]
+    - annual-financial-reports
+  - insights
+    - research
+      - [slug]
+    - insights
+      - [slug]
+  - latest
+    - newsroom
+      - [slug]
+    - events
+      - [slug]
+  - join-us
+  - feed
+    - [slug]
+  - donate
+
+-->
+<main>
+	<slot />
+</main>
+<!-- footer -->
