6bdd1fd5cdc9cc0f0468b058e94bd463a359920c
etczrn <etczrn@gmail.com>
2024-03-12 23:45:04 +0900

    what we do section -> split layout 컴포넌트 교체

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -17,6 +17,7 @@
 	linkText="See all Research"
 	articles={research}
 />
+
 <MainSection
 	title="Insights"
 	subtitle="New Insights"
@@ -24,6 +25,7 @@
 	linkText="See all insights"
 	articles={insights}
 />
+
 <section class="who-we-are">
 	<MainSplit>
 		<h1 slot="title">Who we are</h1>
@@ -41,16 +43,16 @@
 </section>
 
 <section class="what-we-do">
-	<div class="split-layout">
-		<h1>What we do</h1>
-		<div class="description">
+	<MainSplit>
+		<h1 slot="title">What we do</h1>
+		<svelte:fragment slot="paragraph">
 			<p>
 				Solutions for Our Climate works towarda society that protects people from the dangers of
 				climate change.
 			</p>
 			<MainLink href="/what-we-do">See More</MainLink>
-		</div>
-	</div>
+		</svelte:fragment>
+	</MainSplit>
 </section>
 
 <section class="our-people"></section>
@@ -58,64 +60,6 @@
 <section class="newsletter"></section>
 
 <style>
-	.split-layout {
-		max-width: var(--max-w-screen-2xl);
-		margin-inline: auto;
-		display: grid;
-		grid-template-columns: minmax(22.5rem, 1fr) 1fr;
-		grid-template-areas: 'title description';
-		padding: 2.3125em 2.25rem;
-		/* min-height: 36.875rem; */
-	}
-
-	@media (width <= 48rem) {
-		.split-layout {
-			grid-template-columns: 1fr;
-			grid-template-areas: 'title' 'description';
-			padding: 1.5rem;
-			max-width: var(--max-w-prose);
-			grid-template-rows: min-content 1fr;
-		}
-	}
-
-	.split-layout > h1 {
-		grid-area: title;
-		font-size: var(--text-7xl);
-		letter-spacing: var(--tracking-tighter);
-		margin-right: 2.25rem;
-	}
-
-	@media (width <= 48rem) {
-		.split-layout > h1 {
-			font-size: var(--text-6xl);
-			margin-bottom: 2.5rem;
-		}
-	}
-
-	.split-layout .description {
-		margin-top: 1.25rem;
-		letter-spacing: var(--tracking-tight);
-		max-width: var(--max-w-ch-sm);
-		font-size: var(--text-2xl);
-		line-height: var(--leading-snug);
-	}
-
-	@media (width <= 48rem) {
-		.split-layout .description {
-			margin-top: 0;
-		}
-	}
-
-	.split-layout .description p {
-		margin-bottom: 1.16rem;
-	}
-
-	@media (width <= 48rem) {
-		.split-layout .description p {
-			margin-bottom: 1.5rem;
-		}
-	}
-
 	.who-we-are {
 		background-color: var(--color-gray-light);
 		position: relative;
