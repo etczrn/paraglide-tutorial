43c1a5c70c644d5433000a8287f20a221fde0864
etczrn <etczrn@gmail.com>
2024-03-17 22:37:06 +0900

    스벨트 애니메이션을 css로 변경

--- src/lib/components/public/invew.svelte
+++ src/lib/components/public/invew.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { inview, type ObserverEventDetails, type Options } from 'svelte-inview';
-	import { fly } from 'svelte/transition';
 
 	let isInView: boolean = false;
 	const options: Options = {
@@ -13,12 +12,33 @@
 	};
 </script>
 
-<div use:inview={options} on:inview_change={handleChange}>
+<div use:inview={options} on:inview_change={handleChange} class="inview">
 	{#if isInView}
-		<div class:animate={isInView} in:fly={{ y: '10vw' }}>
+		<div class:active={isInView}>
 			<slot />
 		</div>
-	{:else}
-		<slot />
 	{/if}
 </div>
+
+<style>
+	div:not(.inview) {
+		opacity: 0;
+	}
+
+	.active {
+		animation: fadeInY 0.5s ease-out both;
+		animation-delay: 0s;
+		opacity: 1;
+	}
+
+	@keyframes fadeInY {
+		0% {
+			opacity: 0;
+			transform: translateY(10vw);
+		}
+		100% {
+			opacity: 1;
+			transform: translateY(0);
+		}
+	}
+</style>
